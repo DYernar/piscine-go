19693839742d08339fd68e4e3b47e6e5919db2e5
DYernar <eduisebai.1@gmail.com>
2019-10-16 05:38:31 +0600

    sdfsdf

--- printcomb2.go
+++ printcomb2.go
@@ -5,7 +5,11 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
-			z01.PrintRune(' ')
+			if i == '0' && j == '0' {
+
+			} else {
+				z01.PrintRune(' ')
+			}
 			z01.PrintRune(i)
 			z01.PrintRune(j)
 			z01.PrintRune(' ')
