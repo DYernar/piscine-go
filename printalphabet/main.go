5b25b6be797d11e515138a6d3b30a8d9fa6e0316
DYernar <eduisebai.1@gmail.com>
2019-10-16 01:57:07 +0600

    sdfds

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-for i := 97; i<=122; i++{
-	fmt.Printf(string(i))
-}
-fmt.Println()
+	for i := 97; i<=122; i++{
+		fmt.Printf(string(i))
+	}
+	fmt.Println()
 }
