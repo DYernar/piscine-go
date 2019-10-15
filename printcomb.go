07941a5aaaf3cf63ace101b6eed019168176bd50
DYernar <eduisebai.1@gmail.com>
2019-10-16 04:07:20 +0600

    sdsf

--- printcomb.go
+++ printcomb.go
@@ -2,11 +2,11 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func PrintComb()  {
-	var i, j, k int;
+func PrintComb() {
+	var i, j, k int
 	for i = '0'; i <= '9'; i++ {
 		for j = '0'; j <= '9'; j++ {
-			for k = '0'; k <= '9'; k++{
+			for k = '0'; k <= '9'; k++ {
 				if j < i {
 					if k < j {
 						z01.PrintRune(rune(k))
