22efe7a37c98edc9415d1a5560dd85375458db01
DYernar <eduisebai.1@gmail.com>
2019-10-16 05:49:44 +0600

    sdfdf

--- printcomb2.go
+++ printcomb2.go
@@ -6,18 +6,18 @@ func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
-				for l := '1'; l<= '9'; l++ {
-					if i == '0' && j == '0' && k == '0' && l == '1'{
+				for l := '1'; l <= '9'; l++ {
+					if i == '0' && j == '0' && k == '0' && l == '1' {
 
-						} else {
-							z01.PrintRune(' ')
-						}
-						z01.PrintRune(i)
-						z01.PrintRune(j)
+					} else {
 						z01.PrintRune(' ')
-						z01.PrintRune(k)
-						z01.PrintRune(l)
-						z01.PrintRune(',')
+					}
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(l)
+					z01.PrintRune(',')
 				}
 			}
 		}
