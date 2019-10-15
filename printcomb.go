744524511d2d4c7f24bacd9401254b04e124f774
DYernar <eduisebai.1@gmail.com>
2019-10-16 04:19:38 +0600

    sdsf

--- printcomb.go
+++ printcomb.go
@@ -6,14 +6,12 @@ func PrintComb() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
-				if j < i {
-					if k < j {
-						z01.PrintRune(k)
-						z01.PrintRune(j)
-						z01.PrintRune(i)
-						z01.PrintRune(',')
-						z01.PrintRune(' ')
-					}
+				if i < j && j < k{
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(k)
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
 				}
 			}
 		}
