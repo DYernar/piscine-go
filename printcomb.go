c24999d91bbd7390b6c938b016e8988562b4f005
DYernar <eduisebai.1@gmail.com>
2019-10-16 04:05:16 +0600

    sdsf

---
+++ printcomb.go
@@ -0,0 +1,22 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb()  {
+	var i, j, k int;
+	for i = '0'; i <= '9'; i++ {
+		for j = '0'; j <= '9'; j++ {
+			for k = '0'; k <= '9'; k++{
+				if j < i {
+					if k < j {
+						z01.PrintRune(rune(k))
+						z01.PrintRune(rune(j))
+						z01.PrintRune(rune(i))
+						z01.PrintRune(rune(','))
+						z01.PrintRune(' ')
+					}
+				}
+			}
+		} 
+	}
+}
