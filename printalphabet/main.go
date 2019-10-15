82367abb3f169084362f99b29ba0cd2cbdd55ea2
DYernar <eduisebai.1@gmail.com>
2019-10-16 01:39:38 +0600

    q2t1

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,9 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	// var cha rune = '\n'
 	for i := 97; i<=122; i++{
 		fmt.Printf(string(i))
 	}
-	// fmt.Printf(string(cha))
+	fmt.Printf(string('\n'))
 }
