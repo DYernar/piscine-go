65380d82d1e1f13d369af7a37de6f8702306ee90
DYernar <eduisebai.1@gmail.com>
2019-10-16 04:36:11 +0600

    sdsf

--- printcomb.go
+++ printcomb.go
@@ -10,11 +10,11 @@ func PrintComb() {
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(k)
-					if i == 7 && j == 8 && k == 9 {
-
-					} else {
+					if i != 7 && j != 8 && k != 9{
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
+					} else {
+						
 					}
 				}
 			}
