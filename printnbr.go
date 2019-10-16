a013f214e22dd6d651286ad648d9503bbed7dc89
DYernar <eduisebai.1@gmail.com>
2019-10-17 01:54:34 +0600

    sdfdf

--- printnbr.go
+++ printnbr.go
@@ -3,14 +3,32 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
-	var isnegative bool
 	var strVal string
 	var length int
+	var isnegative bool
 	if n > 0 {
 		isnegative = false
 		for i := 0; i <= 10; i++ {
 			if n != 0 {
-				strVal = string((n%10)+48) + strVal
+				if n == 1 {
+					strVal = "1" + strVal
+				} else if n % 10 == 2 {
+					strVal = "2" + strVal
+				} else if n % 10 == 3 {
+					strVal = "3" + strVal
+				} else if n % 10 == 4  {
+					strVal = "4" + strVal
+				} else if n % 10 == 5 {
+					strVal = "5" + strVal
+				} else if n % 10 == 6 {
+					strVal = "6" + strVal
+				} else if n % 10 == 7 {
+					strVal = "7" + strVal
+				} else if n % 10 == 8 {
+					strVal = "8" + strVal
+				} else {
+					strVal = "9" + strVal
+				}
 				n = (n - (n % 10)) / 10
 				length++
 			}
@@ -20,22 +38,40 @@ func PrintNbr(n int) {
 		n = -n
 		for i := 0; i <= 10; i++ {
 			if n != 0 {
-				strVal = string((n%10)+48) + strVal
+				if n == 1 {
+					strVal = "1" + strVal
+				} else if n % 10 == 2 {
+					strVal = "2" + strVal
+				} else if n % 10 == 3 {
+					strVal = "3" + strVal
+				} else if n % 10 == 4  {
+					strVal = "4" + strVal
+				} else if n % 10 == 5 {
+					strVal = "5" + strVal
+				} else if n % 10 == 6 {
+					strVal = "6" + strVal
+				} else if n % 10 == 7 {
+					strVal = "7" + strVal
+				} else if n % 10 == 8 {
+					strVal = "8" + strVal
+				} else {
+					strVal = "9" + strVal
+				}
 				n = (n - (n % 10)) / 10
 				length++
 			}
 		}
+	}
+	if isnegative {
+		strVal = "-" + strVal
+		length++
+	} else if !isnegative {
+
 	} else {
 		strVal = "0"
+		length = 1
 	}
-	if !isnegative {
-		for j := 0; j < length; j++ {
-			z01.PrintRune(rune(strVal[j]))
-		}
-	} else {
-		strVal = "-" + strVal
-		for j := 0; j < length; j++ {
-			z01.PrintRune(rune(strVal[j]))
-		}
+	for j := 0; j <length; j++ {
+		z01.PrintRune(rune(strVal[j]));
 	}
 }
