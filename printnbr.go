65c646a980bb814909dcb17cf9dff111b38b9441
DYernar <eduisebai.1@gmail.com>
2019-10-17 01:04:36 +0600

    sdfdsf

--- printnbr.go
+++ printnbr.go
@@ -5,12 +5,14 @@ import "github.com/01-edu/z01"
 func PrintNbr(n int) {
 	var isnegative bool
 	var strVal string
+	var length int
 	if n > 0 {
 		isnegative = false
 		for i := 0; i <= 10; i++ {
 			if n != 0 {
 				strVal = string((n%10)+48) + strVal
 				n = (n - (n % 10)) / 10
+				length++
 			}
 		}
 	} else if n < 0 {
@@ -20,18 +22,19 @@ func PrintNbr(n int) {
 			if n != 0 {
 				strVal = string((n%10)+48) + strVal
 				n = (n - (n % 10)) / 10
+				length++
 			}
 		}
 	} else {
 		strVal = "0"
 	}
 	if !isnegative {
-		for j := 0; j < len(strVal); j++ {
+		for j := 0; j < length; j++ {
 			z01.PrintRune(rune(strVal[j]))
 		}
 	} else {
 		strVal = "-" + strVal
-		for j := 0; j < len(strVal); j++ {
+		for j := 0; j < length; j++ {
 			z01.PrintRune(rune(strVal[j]))
 		}
 	}
