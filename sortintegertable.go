f01ab4a1201bb39cf0745dd9155ee0d6da8f9993
DYernar <eduisebai.1@gmail.com>
2019-10-17 23:17:48 +0600

    sort

--- sortintegertable.go
+++ sortintegertable.go
@@ -1,12 +1,20 @@
 package piscine
 
-func SortIntegerTable(table []int) {
-	n := len(table)
-	for i := 0; i < n; i++ {
-		for j := i; j < n; j++ {
-			if table[i] > table[j] {
-				Swap(&table[i], &table[j])
+func SortIntegerTable(table []int) []int {
+	i := 0
+	for range table {
+		i++
+	}
+	for k := 0; k < i; k++ {
+		for j := 0; j < i; j++ {
+			if j != 0 {
+				if table[j-1] > table[j] {
+					temp := table[j-1]
+					table[j-1] = table[j]
+					table[j] = temp
+				}
 			}
 		}
 	}
+	return table
 }
