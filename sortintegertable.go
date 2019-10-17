d1157f2625615001efa79e0a2718a22b3a015bb7
DYernar <eduisebai.1@gmail.com>
2019-10-17 22:59:55 +0600

    sort

---
+++ sortintegertable.go
@@ -0,0 +1,22 @@
+package piscine
+
+func SortIntegerTable(table []int) []int {
+	i := 0
+	for range table {
+		i++
+	}
+	for k := 0; k < i; k++ {
+		for j := 0; j < i; j++ {
+			if j != 0 {
+				if table[j-1] > table[j] {
+					temp := table[j-1]
+					table[j-1] = table[j]
+					table[j] = temp
+				} else {
+					break
+				}
+			}
+		}
+	}
+	return table
+}
