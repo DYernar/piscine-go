aad20c1ddde599415be57d2ded616c5807b6c50a
DYernar <eduisebai.1@gmail.com>
2019-10-22 00:12:58 +0600

    sdf

--- isprime.go
+++ isprime.go
@@ -1,12 +1,9 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	if nb < 0 {
+	if nb%2 == 0 || nb%3 == 0 || nb%5 == 0 || nb%7 == 0 || nb%13 == 0 {
 		return false
-	}
-	if nb%2 != 0 || nb%3 != 0 || nb%5 != 0 || nb%7 != 0 || nb%13 != 0 {
-		return true
 	} else {
-		return false
+		return true
 	}
 }
