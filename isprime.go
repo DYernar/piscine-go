7472482498344ff76b045e9da6b148abba19ad4d
DYernar <eduisebai.1@gmail.com>
2019-10-22 00:39:24 +0600

    fsd

--- isprime.go
+++ isprime.go
@@ -5,7 +5,7 @@ func IsPrime(nb int) bool {
 		return false
 	} else if nb == 2 || nb == 3 || nb == 5 || nb == 7 {
 		return true
-	} else if nb%2 == 0 || nb%3 == 0 || nb%5 == 0 || nb%7 == 0 || nb%13 == 0 {
+	} else if nb%2 == 0 || nb%3 == 0 || nb%5 == 0 || nb%7 == 0 || nb%13 == 0 || nb%17 == 0 || nb%19 == 0 || nb%79 == 0 {
 		return false
 	} else {
 		return true
