7582d522d55bf7a20bc223a8cc41083154882721
DYernar <eduisebai.1@gmail.com>
2019-10-22 23:54:48 +0600

    capitalize.go

--- capitalize.go
+++ capitalize.go
@@ -4,22 +4,19 @@ func Capitalize(s string) string {
 	var arr string
 	var nextUpper bool
 	for index, item := range s {
-		if (item > 47 && item < 58) || (item > 64 && item < 91) || (item > 96 && item < 123) && nextUpper != true {
-			nextUpper = false
+		if (item > 47 && item < 58) || (item > 64 && item < 91) || (item > 96 && item < 123) {
+			if index == 0 {
+				arr = arr + ToUpper(string(item))
+			} else {
+				if nextUpper {
+					arr = arr + ToUpper(string(item))
+					nextUpper = false
+				} else {
+					arr = arr + ToLower(string(item))
+				}
+			}
 		} else {
-			nextUpper = true
-		}
-		if index == 0 {
-			nextUpper = true
-		}
-
-		if nextUpper && !(item > 32 && item < 'A') || (item > 'Z' && item < 'a') || (item > 'z' && item < 128) {
-			arr = arr + ToUpper(string(item))
-			nextUpper = false
-		} else {
-			arr = arr + ToLower(string(item))
-		}
-		if item == ' ' || item == '+' || item == '.' || item == ':' || item == ';' {
+			arr = arr + string(item)
 			nextUpper = true
 		}
 	}
