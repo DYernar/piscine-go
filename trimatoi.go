e664cf2beaf269fb90a85ff502cd91e5e21c2f25
DYernar <eduisebai.1@gmail.com>
2019-10-22 19:40:12 +0600

    q5e3

---
+++ trimatoi.go
@@ -0,0 +1,14 @@
+package piscine
+
+func TrimAtoi(str string) int {
+	var arr string
+	for _, item := range str {
+		if item == '-' && len(arr) == 0 {
+			arr = string(item) + arr
+		}
+		if item >= 48 && item <= 57 {
+			arr = arr + string(item)
+		}
+	}
+	return Atoi(arr)
+}
