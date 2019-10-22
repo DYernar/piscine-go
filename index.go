e9cb2f995d14e97d03b0cecbc0f2ee5c5ccd1bae
DYernar <eduisebai.1@gmail.com>
2019-10-22 21:39:43 +0600

    index

---
+++ index.go
@@ -0,0 +1,23 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	var length int
+	var lengthToFind int
+	foundIndex := 0
+	for range toFind {
+		lengthToFind++
+	}
+	for range s {
+		length++
+	}
+
+	for i := 0; i < lengthToFind; i++ {
+		for j := 0; j < length; j++ {
+			if toFind[i] == s[j] {
+				foundIndex = j
+			}
+		}
+	}
+
+	return foundIndex
+}
