7fbc9d1c7ef08707684603cdf1065758d99ab758
DYernar <eduisebai.1@gmail.com>
2019-10-22 23:59:51 +0600

    isalpha.go

---
+++ isalpha.go
@@ -0,0 +1,18 @@
+package piscine
+
+func IsAlpha(str string) bool {
+	var lengthOfString int
+	var lengthOfAlphas int
+	for _, item := range str {
+		if !((item > 47 && item < 58) || (item > 64 && item < 91) || (item > 96 && item < 123)) {
+			lengthOfAlphas++
+		}
+		lengthOfString++
+	}
+	if lengthOfString == lengthOfAlphas {
+		return true
+	} else {
+		return false
+	}
+
+}
