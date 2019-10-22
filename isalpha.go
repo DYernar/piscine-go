b6a39341ee20a6195959fda4b0dc78cee34f3ebe
DYernar <eduisebai.1@gmail.com>
2019-10-23 00:04:45 +0600

    isalpha.go

--- isalpha.go
+++ isalpha.go
@@ -2,14 +2,14 @@ package piscine
 
 func IsAlpha(str string) bool {
 	var lengthOfString int
-	var lengthOfAlphas int
+	var notAlpha int
 	for _, item := range str {
-		if !((item > 47 && item < 58) || (item > 64 && item < 91) || (item > 96 && item < 123)) {
-			lengthOfAlphas++
+		if (item > 47 && item < 58) || (item > 64 && item < 91) || (item > 96 && item < 123) {
+			notAlpha++
 		}
 		lengthOfString++
 	}
-	if lengthOfString == lengthOfAlphas {
+	if notAlpha > 0 {
 		return false
 	} else {
 		return true
