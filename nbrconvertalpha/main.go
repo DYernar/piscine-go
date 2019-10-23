7b7cc143fc2181e48b2bb3705ede89dbaf628118
DYernar <eduisebai.1@gmail.com>
2019-10-23 19:08:23 +0600

    convertalpha.go

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -3,9 +3,50 @@ package main
 import (
 	"os"
 
-	piscine ".."
 	"github.com/01-edu/z01"
 )
+
+func Atoi(s string) int {
+	var i int
+	var isnegative bool = false
+	for _, letter := range s {
+		if letter == '-' {
+			if isnegative || i != 0 {
+				return 0
+			} else {
+				isnegative = true
+			}
+		} else if letter == '1' {
+			i = (i * 10) + 1
+		} else if letter == '2' {
+			i = (i * 10) + 2
+		} else if letter == '3' {
+			i = (i * 10) + 3
+		} else if letter == '4' {
+			i = (i * 10) + 4
+		} else if letter == '5' {
+			i = (i * 10) + 5
+		} else if letter == '6' {
+			i = (i * 10) + 6
+		} else if letter == '7' {
+			i = (i * 10) + 7
+		} else if letter == '8' {
+			i = (i * 10) + 8
+		} else if letter == '9' {
+			i = (i * 10) + 9
+		} else if letter == '0' {
+			i = i * 10
+		} else if letter == ' ' {
+			return 0
+		} else {
+			return 0
+		}
+	}
+	if isnegative {
+		i *= -1
+	}
+	return i
+}
 
 func main() {
 	args := os.Args
@@ -16,17 +57,17 @@ func main() {
 
 	for i := 1; i < length+1; i++ {
 		if args[1] == "--upper" {
-			val := piscine.Atoi(args[i])
+			val := Atoi(args[i])
 			if val > 0 && val < 27 {
-				newval := rune(piscine.Atoi(args[i]))
+				newval := rune(Atoi(args[i]))
 				z01.PrintRune(newval + 64)
 			} else if i != 1 {
 				z01.PrintRune(' ')
 			}
 		} else {
-			val := piscine.Atoi(args[i])
+			val := Atoi(args[i])
 			if val > 0 && val < 27 {
-				newval := rune(piscine.Atoi(args[i]))
+				newval := rune(Atoi(args[i]))
 				z01.PrintRune(newval + 96)
 			} else {
 				z01.PrintRune(' ')
