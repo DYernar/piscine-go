75f89b723d8f0b8696150be6c9ea288cfdb4dcfc
DYernar <eduisebai.1@gmail.com>
2019-10-24 18:57:59 +0600

    mkrange

--- makerange.go
+++ makerange.go
@@ -4,8 +4,8 @@ func MakeRange(min, max int) []int {
 	arr := make([]int, max-min)
 
 	if !(min >= max) {
-		for i := min; i < max; i++ {
-			arr[i] = i
+		for i := 0; i < max-min; i++ {
+			arr[i] = i + min
 		}
 	}
 	return arr
