1f2c6202796abaab23e3f31ac1047104a8334067
DYernar <eduisebai.1@gmail.com>
2019-10-28 17:27:57 +0600

    sdf

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,21 +1,51 @@
 package piscine
 
+import "fmt"
+
 func SplitWhiteSpaces(str string) []string {
-	var length int
+	spaces := 0
+	totalLength := 0
 	for range str {
-		length++
+		totalLength++
 	}
-	arr := make([]string, length)
-	var indWords string
-	i := 0
 	for _, item := range str {
-		if item != ' ' && item != '\n' {
-			indWords = indWords + string(item)
-		} else {
-			arr[i] = indWords
-			i++
-			indWords = ""
+		if item == ' ' {
+			spaces++
 		}
 	}
+	whiteSpaces := make([]int, spaces+2)
+	i := 0
+	for index, item := range str {
+		if index == 0 {
+			whiteSpaces[i] = index
+			i++
+		}
+		if item == ' ' {
+			whiteSpaces[i] = index
+			i++
+		}
+	}
+	whiteSpaces[i] = totalLength
+	lengthOfArray := 0
+	for range whiteSpaces {
+		lengthOfArray++
+	}
+	fmt.Println(str[0])
+	arr := make([]string, spaces+1)
+	for j := 0; j < lengthOfArray-1; j++ {
+		if j == 0 {
+			for k := whiteSpaces[j]; k < whiteSpaces[j+1]; k++ {
+				arr[j] = arr[j] + string(str[k])
+			}
+		} else {
+			if whiteSpaces[j+1]-whiteSpaces[j] != 1 {
+				for k := whiteSpaces[j] + 1; k < whiteSpaces[j+1]; k++ {
+					arr[j] = arr[j] + string(str[k])
+				}
+			}
+		}
+
+	}
+
 	return arr
 }
